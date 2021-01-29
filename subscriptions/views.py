5d9d8d887f3308af42554a1b3a5651bde6538382
FabianMarsh <fabian.marsh00@gmail.com>
2021-01-29 14:05:01 +0000

    added subscriptions views and templates

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Subscriptions
 
-# Create your views here.
+
+def all_subscriptions(request):
+    """ A view to show all subscriptions """
+
+    subscriptions = Subscriptions.objects.all()
+
+    context = {
+        "subscriptions": subscriptions,
+    }
+
+    return render(request, "subscriptions/subscriptions.html", context)
